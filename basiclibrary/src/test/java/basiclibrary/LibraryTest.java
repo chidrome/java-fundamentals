34e82c2a901a7bd3a6c2b383f7b784e25b8a9a7d
chidrome <chidrome@gmail.com>
2019-05-30 13:45:04 -0700

    method and testing complete

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -47,4 +47,16 @@ public class LibraryTest {
         assertEquals("LowsestAvg is: ",57.0, average, 0.01);
 
     }
+
+    @Test
+    public void testAnalyzeWeatherDataMethod(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        analyzeWeatherData(weeklyMonthTemperatures);
+    }
 }
