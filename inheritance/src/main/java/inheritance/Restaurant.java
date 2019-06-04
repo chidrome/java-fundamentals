bd3220e7e70c18259b45d6c759cbfb6523716be5
chidrome <chidrome@gmail.com>
2019-06-04 13:53:53 -0700

    methods created for restaurants

---
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -0,0 +1,35 @@
+package inheritance;
+
+
+public class Restaurant {
+    private String name;
+    private int rating;
+    private String price;
+
+    // restaurant constructor
+    public Restaurant(String name, int rating, String price){
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    // toString method
+    public String toString() {
+        return String.format("This restaurant %s has a %d star rating and %s pricing.", this.getName(), this.getRating(), this.getPrice());
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+
+}
